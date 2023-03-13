0d3b236a5839a49c44e280b13ce5ccdfae7aac2f
Ilya <you@example.com>
2023-03-13 11:17:54 +0500

    Linking pages

--- myblog/urls.py
+++ myblog/urls.py
@@ -3,4 +3,5 @@ from .views import MainView
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
+    path('blog/<slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.views import View
-# Create your views here.
+from django.core.paginator import Paginator
+from .models import Post
 
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            'myblog/home.html'
-        )
+        posts = Post.objects.all()
+        paginator = Paginator(posts, 6)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'myblog/home.html', context={
+            'page_obj': page_obj
+        })
+
+class PostDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        return render(request, 'myblog/post_detail.html', context={
+            'post': post
+    })
